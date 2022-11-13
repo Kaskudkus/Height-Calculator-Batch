8d211e0f6cbf734494a12051cf6914e3529b3e5d
Kaskudkus <93003836+Kaskudkus@users.noreply.github.com>
2022-11-13 12:54:37 +0100

    1.5 Update

--- Batch File/hc.bat
+++ Batch File/hc.bat
@@ -1,11 +1,32 @@
 @echo off
-title Height Calculator
 color 2
-goto cmorf
+goto Menu
+
+:: Boredom Gaming
+:Menu
+    title Height Calculator - Main Menu
+    echo.
+    echo.
+    echo.
+    echo Kaskudek's
+    echo                           Height Calculator
+    echo                     Made with Visual Studio Code
+    echo                           on SupRize Engine
+    echo.
+    echo.
+    echo.
+    echo Would you like to enter?
+    choice /c YN
+        if %errorlevel% equ 1 goto cmorf
+        if %errorlevel% equ 2 exit
 
 :: I love Bloodrayne
 :cmorf
     cls
+    title Height Calculator
+    echo.
+    echo.
+    echo.
     echo Would you like to use Centimeters or Feet?
     echo.
     echo.
@@ -14,27 +35,110 @@ goto cmorf
     echo.
     echo 2. Feet
     echo.
-    echo 3. Exit
+    echo 3. Check Your Height
+    echo.
+    echo 4. Exit
 	echo.
-    set /p choose=Choose: 
-        :: Yandere Simulator moment
-        if %choose%==1 goto cm
-        if %choose%==2 goto f
-        if %choose%==3 exit
-        if not %choose%==1 goto uhoh
-        if not %choose%==2 goto uhoh
+    choice /c 12345
+        :: Fresh new code
+        if %errorlevel% equ 1 goto cm
+        if %errorlevel% equ 2 goto f
+        if %errorlevel% equ 3 goto check
+        if %errorlevel% equ 4 goto exit
+
+:exit
+    exit
+
+:: Height Check function
+:check
+    cls
+    title Height Calculator - Checking
+    echo.
+    echo.
+    echo.
+    echo Your Height (in Centimeters):     %Writec%
+    echo.
+    echo Your Height (in Feet):            %Writef%
+    echo.
+    echo.
+    echo.
+    echo Press any button to continue
+pause >nul
+goto cmorf
+
+:: useless saving mechanic
+:save
+    cls
+    title Height Calculator - Saving
+    echo.
+    echo.
+    echo.
+    echo Wanna save?
+    echo.
+    choice /c YN
+        if %errorlevel% equ 1 goto save2
+        if %errorlevel% equ 2 goto cmorf
+
+        :save2
+            cls
+            echo Name Your Save
+            echo.
+            set /p file=Save Name: 
+            (
+                echo Centimeters %Writec%
+                echo Feet        %Writef%
+            )>%file%.height
+            title Height Calculator - Saved
+            echo Saved your height
+            ping localhost -n 2 >nul
+            echo Would you like to continue?
+            choice /c YN
+                if %errorlevel% equ 1 goto cmorf
+                if %errorlevel% equ 2 exit
+
+:: useless load mechanic
+:load
+    cls
+    title Height Calculator - Loading
+    echo.
+    echo.
+    echo.
+    echo Type in the name of your savefile.
+    echo.
+    set /p savename=Save File Name: 
+        if exist "%file%.height" goto load2
+        echo Skill issue
+        ping localhost -n 2 >nul
+        goto cmorf
+        
+        :: I love Bloodrayne -Kaskudek
+        :load2
+            cls
+            < %file%.height (
+                set /p Writec=
+                set /p Writef=
+            )
+            title Height Calculator - Loaded
+            echo Loaded (for some reason)
 
 :cm
     :: "WHAT THE FUCK IS A CENTIMETER?" -Any american man
     cls
+    title Height Calculator - Centimeters
+    echo.
+    echo.
+    echo.
     echo Enter your Height (in cm)
-    set /p Write=Write:
+    set /p Writec=Write:
         goto yourheightcm
     
         :: That one meme
         :yourheightcm
             cls
-            echo your height is %Write% cm.
+            echo.
+            echo.
+            echo.
+            echo Your height is %Writec% cm.
             echo.
             echo.
             echo Press any key to continue
@@ -44,14 +148,21 @@ goto cmorf
 :f
     :: Eryczek
     cls
+    title Height Calculator - Feet
+    echo.
+    echo.
+    echo.
     echo Enter your height (in feet)
-    set /p Write=Write:
+    set /p Writef=Write:
         goto yhf
 
         :: I hate Feet Fetishists -Any European man
         :yhf
             cls
-            echo your height is %Write%.
+            echo.
+            echo.
+            echo.
+            echo your height is %Writef%.
             echo.
             echo.
             echo Press any key to continue
@@ -59,20 +170,19 @@ goto cmorf
             goto cmorf 
 
 :uhoh
-    :: You stupid
+    :: I'm stupid
     cls
     echo Whoops, looks like Fucked Up.
     echo.
     echo.
     echo Wanna go back?
     echo.
-    set /p yn=[y/n]: 
-        if %yn%==y goto cmorf
-        if %yn%==n exit
-        if not %yn%==y goto bruh
-        if not %yn%==n goto bruh
+    choice /c YN
+        if %errorlevel% equ 1 goto cmorf
+        if %errorlevel% equ 2 bruh
 
         :bruh
+            :: Scrapped but still keeping it
             cls
             echo bruh.
             ping localhost -n 2 >nul
